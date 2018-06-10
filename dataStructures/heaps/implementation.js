fad31ba47a1345b6dc0aeb643b5232b27c6f0aea
Matthew Chang <matthewchang3150@hotmail.com>
2018-06-09 17:08:31 -0700

    Extended heap functions with priority queue functionality

--- dataStructures/heaps/implementation.js
+++ dataStructures/heaps/implementation.js
@@ -1,3 +1,5 @@
+// All heaps index start at 1. The 0 index is reserved for storing the heap size.
+
 // O (log N)
 const maxHeapify = (arr, i) => {
   const l = i * 2;
@@ -27,14 +29,40 @@ const buildMaxHeap = (arr) => {
   return arr;
 };
 
+
+// Priority Queue
+/* Similiar to a queue but instead of FIFO, the element with the the largest/smallested key
+gets extracted first */
 const heapExtractMax = (arr) => {
   if (arr[0] < 1) throw new Error('Heap underflow');
   const max = arr[1];
-  arr[1] = arr[arr[0]];
+  const heapSize = arr[0];
+  arr[1] = arr[heapSize];
   arr[0]--;
   maxHeapify(arr, 1);
   return max;
 };
+
+const heapIncreaseKey = (arr, i, key) => {
+  if (key < arr[i]) throw new Error('New key is smaller than current key');
+  arr[i] = key;
+  let parent = Math.floor(i / 2);
+  while (i > 1 && arr[parent] < arr[i]) {
+    const temp = arr[i];
+    arr[i] = arr[parent];
+    arr[parent] = temp;
+    i = parent;
+    parent = Math.floor(i / 2);
+  }
+};
+
+const maxHeapInsert = (arr, key) => {
+  arr[0]++;
+  const heapSize = arr[0];
+  arr[heapSize] = -Infinity;
+  heapIncreaseKey(arr, heapSize, key);
+};
+
 
 module.exports = {
   maxHeapify,
