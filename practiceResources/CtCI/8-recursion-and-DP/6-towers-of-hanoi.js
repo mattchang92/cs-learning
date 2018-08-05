f5047a14f3faf85f211a4c18a4457591ded06b9a
Matthew Chang <matthewchang3150@hotmail.com>
2018-08-05 12:43:39 -0700

    Added class based solution to hanoi

--- practiceResources/CtCI/8-recursion-and-DP/6-towers-of-hanoi.js
+++ practiceResources/CtCI/8-recursion-and-DP/6-towers-of-hanoi.js
@@ -1,26 +1,20 @@
 /* eslint-disable no-extend-native, func-names */
+Array.prototype.last = function () {
+  return this.length ? this.slice(-1)[0] : Infinity;
+};
 
 const hanoi = (disks) => {
-  Array.prototype.last = function () {
-    return this.length ? this.slice(-1)[0] : Infinity;
-  };
   const towers = [disks, [], []];
 
   const solve = (src, dest, n, buffer) => {
-    console.log(towers);
     if (n === 1 && (src.last() < dest.last())) {
       const disk = src.pop();
       return dest.push(disk);
     } else if (n === 2) {
-      if (src.last() < buffer.last()) {
-        buffer.push(src.pop());
-      }
-      if (src.last() < dest.last()) {
-        dest.push(src.pop());
-      }
-      if (buffer.last() < dest.last()) {
-        dest.push(buffer.pop());
-      }
+      if (src.last() < buffer.last()) buffer.push(src.pop());
+      if (src.last() < dest.last()) dest.push(src.pop());
+      if (buffer.last() < dest.last()) dest.push(buffer.pop());
+
       return;
     }
 
@@ -36,28 +30,40 @@ const hanoi = (disks) => {
 };
 
 
-// Solution without size checks
-// const hanoi = (disks) => {
-//   const towers = [disks, [], []];
+// Class based solution
+class Tower {
+  constructor(disks) {
+    this.disks = disks || [];
+  }
 
-//   const solve = (src, dest, n, buffer) => {
-//     if (n === 1) {
-//       const disk = src.pop();
-//       return dest.push(disk);
-//     } else if (n === 2) {
-//       const small = src.pop();
-//       const big = src.pop();
-//       dest.push(big);
-//       return dest.push(small);
-//     }
+  moveTopTo(dest) {
+    if (this.disks.length && this.disks.last() < dest.disks.last()) {
+      dest.disks.push(this.disks.pop());
+    }
+  }
 
-//     solve(src, buffer, n - 1, dest);
-//     const disk = src.pop();
-//     dest.push(disk);
-//     solve(buffer, dest, n - 1, src);
-//   };
+  moveStack(dest, buffer, n) {
+    if (n === 1) {
+      this.moveTopTo(dest);
+    } else if (n === 2) {
+      this.moveTopTo(buffer);
+      this.moveTopTo(dest);
+      buffer.moveTopTo(dest);
+    } else {
+      this.moveStack(buffer, dest, n - 1);
+      this.moveTopTo(dest);
+      buffer.moveStack(dest, this, n - 1);
+    }
+  }
+}
 
-//   solve(towers[0], towers[2], disks.length, towers[1]);
+const hanoiSolver = (disks) => {
+  const towers = [];
+  for (let i = 0; i < 3; i++) {
+    const tower = i === 0 ? new Tower(disks) : new Tower();
+    towers[i] = tower;
+  }
 
-//   return towers;
-// };
+  towers[0].moveStack(towers[2], towers[1], disks.length);
+  return towers;
+};
